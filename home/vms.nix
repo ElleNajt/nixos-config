513e1ac3c84578a22d58255142a93a818fbbd43a
Elle Najt <lnajt4@gmail.com>
2024-08-10 13:05:04 -0400

    Move spice stuff to system pkgs

--- home/vms.nix
+++ home/vms.nix
@@ -1,9 +1,4 @@
 { config, lib, pkgs, ... }:
 with lib; {
-
   options = { elle.is_vm = mkEnableOption "is_vm"; };
-
-  config = mkIf config.elle.is_vm {
-    home.packages = with pkgs; [ spice-gtk spice-vdagent davfs2 ];
-  };
 }
--- system/vms.nix
+++ system/vms.nix
@@ -5,6 +5,9 @@ with lib; {
   options = { elle.is_vm = mkEnableOption "is_vm"; };
 
   config = mkIf config.elle.is_vm {
+
+    environment.systemPackages = with pkgs; [ spice-gtk spice-vdagent davfs2 ];
+
     services.spice-webdavd.enable = true;
     services.spice-vdagentd.enable = true;
     services.qemuGuest.enable = true;
