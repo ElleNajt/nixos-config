f7c68f6d47d7f61af13c98a6d142eed2fcab9a0f
Elle Najt <lnajt4@gmail.com>
2024-08-11 09:12:13 -0400

    Add emacs desktop items, tex
    also aspens emacs everywhere

--- home/emacs.nix
+++ home/emacs.nix
@@ -9,8 +9,29 @@
 
   home.packages = with pkgs; [
 
-    emacs-git
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        capt-of collection-fontsrecommended dvipng fancyvrb float fncychap
+        framed mathpartir needspace parskip scheme-basic semantic tabulary
+        titlesec ulem upquote varwidth wrapfig bussproofs bussproofs-extra;
+    })
 
+    (makeDesktopItem {
+      name = "Doom Emacs";
+      desktopName = "Doom Emacs";
+      icon = "emacs";
+      exec = "${emacs-git}/bin/emacs";
+    })
+
+    (makeDesktopItem {
+      name = "Doom Emacs (Debug Mode)";
+      desktopName = "Doom Emacs (Debug Mode)";
+      icon = "emacs";
+      exec = "${emacs-git}/bin/emacs --debug-init";
+    })
+
+    emacs-git
+    emacsPackages.treesit-grammars.with-all-grammars
     nerdfonts
     ripgrep
     fd
@@ -25,5 +46,26 @@
     cmake
     gnumake
     libtool
+    imagemagick
+
+    # Stolen from aspen
+    (writeShellApplication {
+      name = "edit-input";
+
+      runtimeInputs = [ xdotool xclip ];
+      text = ''
+        set -euo pipefail
+
+        sleep 0.2
+        xdotool key ctrl+a ctrl+c
+        xclip -out -selection clipboard > /tmp/EDIT
+        emacsclient -c /tmp/EDIT
+        xclip -in -selection clipboard < /tmp/EDIT
+        sleep 0.2
+        xdotool key ctrl+v
+        rm /tmp/EDIT
+      '';
+    })
   ];
+
 }
