f260be1f747062e6e12a1ad443b2d9fef63c1c40
Elle Najt <lnajt4@gmail.com>
2024-10-03 22:12:43 -0400

    Add fzf and direnv to shell

--- home/development/shell.nix
+++ home/development/shell.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
 
+    thefuck
     shfmt
     shellcheck
   ];
@@ -13,4 +14,29 @@
   programs.kitty.extraConfig = "confirm_os_window_close -1";
 
   programs.kitty.enable = true;
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+
+    enable = true;
+    shellAliases = {
+      "oops" = "fuck";
+      "nrs" = "sudo ~/code/nixos-config/rebuild-nixos";
+      "hms" = "~/code/nixos-config/rebuild-home";
+      "gc" = "git clone";
+      "fixd" = " xrandr --output Virtual-1 --mode 3024x1890_75.00 --dpi 144";
+    };
+
+  };
 }
