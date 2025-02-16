5dab900d4b48029c237cbd280936c5af8aa41b3a
Elle Najt <lnajt4@gmail.com>
2025-02-16 14:35:56 -0500

    some python packages

--- home/development/python.nix
+++ home/development/python.nix
@@ -4,7 +4,15 @@
   home.packages = with pkgs; [
     texinfo
     pkgs.python313
+    pkgs.black
+    pkgs.isort
     # pkgs.python313Packages.pip
     pkgs.basedpyright
+
+    python3Packages.ipdb
+    python3Packages.jupyter_core
+    python3Packages.jupyterlab
+    python3Packages.notebook
+    python3Packages.ipykernel
   ];
 }
